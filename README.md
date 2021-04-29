3eed457e1f3bc7beba4df981df403e0fb7d3b53b
Stuey61296 <stuartcbarclay@gmail.com>
2021-04-29 23:40:02 +0200

    Updated readme and isAuth

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # messaging-app
+
 Messaging App for BBD Grad Program
+
+
+## Server
+
+### Auth on server
+
+Auth implemented with jwt. Can get google plugin for it but for now needs Authorization header and a bearer + token as a value.
--- server/util/isAuth.js
+++ server/util/isAuth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
     error.statusCode = 401;
     throw error;
   }
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(/\s/g)[1];
   let decodedToken = "";
   try {
     decodedToken = jwt.verify(token, SECRET);
