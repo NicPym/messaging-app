8b5a93ec8a982ecd0e4c9088f944de4d2a1bd3b2
Stuey61296 <stuartcbarclay@gmail.com>
2021-04-29 23:28:51 +0200

    Update testRoutes.js

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,16 +1,45 @@
 const api = require("express").Router();
+const jwt = require("jsonwebtoken");
 const path = require("path");
 const root = require("../util/root");
 const authenticated = require(path.join(root, "util", "isAuth"));
+const { SECRET } = require(path.join(root, "util", "constants"));
 
 api.get("/", (req, res, next) => {
-  res.status(200).json({ message: "[testRoutes.js] Hello From The API" });
-});
-
-api.get("/auth", authenticated, (req, res, next) => {
   res
     .status(200)
-    .json({ message: "[testRoutes.js] Hello From The API With Auth" });
+    .json({
+      message: "[testRoutes.js] Hello From The API",
+      success: true,
+      data: null,
+    });
+});
+
+api.post("/auth", authenticated, (req, res, next) => {
+  res.status(200).json({
+    message: "[testRoutes.js] Hello From The API With Auth",
+    success: true,
+    data: req.token,
+  });
+});
+
+api.get("/getToken", (req, res, next) => {
+  const token = jwt.sign(
+    {
+      email: "Test User",
+      id: 0,
+    },
+    SECRET,
+    {
+      expiresIn: "12h",
+    }
+  );
+
+  res.status(200).json({
+    message: "New Token Created",
+    success: true,
+    data: { token: token },
+  });
 });
 
 module.exports = api;
