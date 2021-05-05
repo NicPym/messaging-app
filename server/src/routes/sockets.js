5a5409738e74c4401fe155dc97f6f93ac6c0c5a6
Nicholas Pym <nicholasp@bbd.co.za>
2021-05-05 09:12:31 +0200

    Formatter sockets.js

--- server/src/routes/sockets.js
+++ server/src/routes/sockets.js
@@ -1,35 +1,35 @@
 const logger = require("../util/winston");
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
 
 module.exports = (server) => {
-    const io = new Server(server, {
-        cors: {
-            origin: "*",
-            methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-            allowedHeaders: ["Content-Type", "Authorization"],
-        }
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    },
+  });
+
+  io.on("connect", (socket) => {
+    logger.log({
+      logger: "info",
+      message: `[sockets.js]\tNew client connected using websocket`,
     });
 
-    io.on("connect", socket => {
-        logger.log({
-            logger: "info",
-            message: `[sockets.js]\tNew client connected using websocket`,
-          });
-    
-        socket.on("disconnect", () => {
-            logger.log({
-                logger: "info",
-                message: `[sockets.js]\tClient disconnected from websocket`,
-              });
-        });
-    
-        socket.on("send message", (message) => {
-            logger.log({
-                logger: "info",
-                message: `[sockets.js]\tMessage Received from websocket: ${message.message}`,
-              });
-        });
+    socket.on("disconnect", () => {
+      logger.log({
+        logger: "info",
+        message: `[sockets.js]\tClient disconnected from websocket`,
+      });
     });
 
-    return io;
+    socket.on("send message", (message) => {
+      logger.log({
+        logger: "info",
+        message: `[sockets.js]\tMessage Received from websocket: ${message.message}`,
+      });
+    });
+  });
+
+  return io;
 };
