12ce61346939de2a3184357abc2aaf51cf55b424
Raymond Walters <raymondw@bbd.co.za>
2021-05-10 22:11:31 +0200

    Refactored FE to be screen size responsive and removed old source

--- app/dist/assets/js/index.js
+++ app/dist/assets/js/index.js
@@ -1,30 +1,30 @@
 //Main Execution of program
-let chats = [];
+let conversations = [];
 let messages = [];
-let currentChat = null;
+let currentConversation = null;
 function init() {
 
     let token = getCookie("token");
     let username = getCookie("username");
     if (token && username) {
-        setInnerHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Select a Chat to see the messages!</label><div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">Logout</label></div>`);
-        loadChats()
+        setInnerHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Select a Conversation to see the messages!</label><div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">Logout</label></div>`);
+        loadConversations()
     }
     else{
-        prependHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Login to see chats and messsages!</label>`)
+        prependHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Login to see conversations and messsages!</label>`)
     }
 }
 
-function loadMessages(chatId) {
-    currentChat = chatId;
-    console.log(`Loading Messages with chatId: ${chatId}`)
+function loadMessages(conversationId) {
+    currentConversation = conversationId;
+    console.log(`Loading Messages with conversationId: ${conversationId}`)
     let messageArr = [];
     let personTo = ""
     let username = getCookie("username");
-    chats.forEach(chat => {
-        if (chat.chatId === chatId) {
-            messageArr = chat.messages;
-            personTo = chat.user1 === username ? chat.user2 : chat.user1;
+    conversations.forEach(conversation => {
+        if (conversation.conversationId === conversationId) {
+            messageArr = conversation.messages;
+            personTo = conversation.user1 === username ? conversation.user2 : conversation.user1;
         }
     });
 
@@ -32,7 +32,7 @@ function loadMessages(chatId) {
     setInnerHtml("personTo", `<div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">${login}</label></div>`)
 
     if(personTo === ""){
-        prependHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Select a Chat to see the messages!</label>`)
+        prependHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Select a Conversation to see the messages!</label>`)
     }else{
         prependHtml("personTo",`<img src="assets/img/profile_picture.png" style="width:40px;height:40px;margin:0 10px 0 20px;"><label style="margin:0 10px;font-size:20px;">${personTo}</label>`)
     }
@@ -42,104 +42,115 @@ function loadMessages(chatId) {
     messageArr.forEach(message => {
         displayMessage(message)
     });
-
-
 }
 
-function displayMessage(message){
+function getMessageReceivedHtml(body, timestamp) {
+    return `
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-row flex-grow-1 align-items-end align-content-start">
+                <div class="card d-flex flex-column" style="margin:20px;max-width:50%;box-shadow:1px 1px 5px grey;">
+                    <div class="d-flex flex-row" style="padding:10px;height:90%;">
+                        <p style="margin:0px;">${body}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-end align-items-start" style="height:10%;padding:5px;">
+                        <p style="font-size:12px;margin:2px 10px;">${timestamp}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
+
+function getMessageSentHtml(body, timestamp) {
+    return `
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-row justify-content-end align-items-end">
+                <div class="card d-flex flex-column" style="margin:20px;max-width:50%;background-color:rgb(220,248,198);box-shadow:1px 1px 5px grey;">
+                    <div class="d-flex flex-row" style="padding:10px;height:90%;">
+                        <p style="margin:0px;">${body}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-end align-items-start" style="height:10%;padding:5px;">
+                        <p style="font-size:12px;margin:2px 10px;">${timestamp}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
+
+function getConversationHtml(id, name) {
+    return `
+        <div class="d-flex align-items-center" style="height:60px;" onclick="loadMessages(${id})">
+            <img src="assets/img/profile_picture2.png" style="width:50px;height:50px;margin:0 10px 0 20px;">
+            <label style="margin:0 10px;font-size:18px;">${name}</label>
+        </div>
+        <hr>`;
+}
+
+function displayMessage(message) {
     let messageHtml = ""
     let username = getCookie("username");
     if (message.sender !== username) {
-        messageHtml += `
-            <div class="d-flex flex-column" style="width:100%;">
-                <div class="d-flex flex-row flex-grow-1 align-items-end align-content-start" style="width:100%;">
-                    <div class="card d-flex flex-column" style="margin:20px;max-width:50%;box-shadow:1px 1px 5px grey;">
-                        <div class="d-flex flex-row" style="padding:10px;width:100%;height:90%;">
-                            <p style="margin:0px;">${message.description}</p>
-                        </div>
-                        <div class="d-flex flex-row justify-content-end align-items-start" style="width:100%;height:10%;padding:5px;">
-                            <p style="font-size:12px;margin:2px 10px;">${message.timestamp}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>`
+        messageHtml += getMessageReceivedHtml(message.description, message.timestamp);
+            
     } else {
-        messageHtml += `
-            <div class="d-flex flex-column" style="width:100%;">
-                <div class="d-flex flex-row justify-content-end align-items-end" style="width:100%;">
-                    <div class="card d-flex flex-column" style="margin:20px;max-width:50%;background-color:rgb(220,248,198);box-shadow:1px 1px 5px grey;">
-                        <div class="d-flex flex-row" style="padding:10px;width:100%;height:90%;">
-                            <p style="margin:0px;">${message.description}</p>
-                        </div>
-                        <div class="d-flex flex-row justify-content-end align-items-start" style="width:100%;height:10%;padding:5px;">
-                            <p style="font-size:12px;margin:2px 10px;">${message.timestamp}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>`
+        messageHtml += getMessageSentHtml(message.description, message.timestamp);
     }
     appendHtml("messages", messageHtml)
 }
 
-function displayChat(chat){
+function displayConversation(conversation) {
     let username = getCookie("username")
-    let person = username === chat.user1 ? chat.user2 : chat.user1;
-    let chatHtml = `
-        <div class="d-flex align-items-center" style="height:10%;width:100%;" onclick="loadMessages(${chat.chatId})">
-            <img src="assets/img/profile_picture2.png" style="width:40px;height:40px;margin:0 10px 0 20px;">
-            <label style="margin:0 10px;font-size:20px;">${person}</label>
-       </div>
-   <hr>`
-    appendHtml("chats", chatHtml);
+    let person = username === conversation.user1 ? conversation.user2 : conversation.user1;
+    appendHtml("conversations", getConversationHtml(conversation.conversationId, person));
 }
-function loadChats() {
-    console.log("Loading Chats")
-    //Todo: api call to server
-    let chat = {
-        chatId: 1,
+
+function loadConversations() {
+    console.log("Loading Conversations")
+    // TODO: api call to server
+    let conversation = {
+        conversationId: 1,
         user1: "Duncan",
         user2: "Stuart",
         messages: [
             {
-                chatId:1,
+                conversationId:1,
                 sender: "Duncan",
                 description: "Hi Stuart",
                 timestamp: "2021-05-08 16:10"
             },
             {
-                chatId:1,
+                conversationId:1,
                 sender: "Stuart",
                 description: "Sup Duncan :)",
                 timestamp: "2021-05-08 16:15"
             }
         ]
     }
-    chats.push(chat);
-    chat = {
-        chatId: 2,
+    conversations.push(conversation);
+    conversation = {
+        conversationId: 2,
         user1: "Raymond",
         user2: "Duncan",
         messages: [
             {
-                chatId:2,
+                conversationId:2,
                 sender: "Raymond",
                 description: "olo Duncan",
                 timestamp: "2021-05-06 11:00"
             },
             {
-                chatId:2,
+                conversationId:2,
                 sender: "Duncan",
                 description: "Booiiiiii",
                 timestamp: "2021-05-07 07:10"
             }
         ]
     }
-    chats.push(chat);
+    conversations.push(conversation);
 
-    setInnerHtml("chats","<hr>");
-    let username = getCookie("username")
-    chats.forEach(chat => {
-        displayChat(chat);
+    setInnerHtml("conversations","<hr>");
+    let username = getCookie("username");
+    conversations.forEach(conversation => {
+        displayConversation(conversation);
     });
 
 }
@@ -147,13 +158,13 @@ function loadChats() {
 function toggleLogin() {
     if (getInnerHtml("loginBtn") === "Login") {
         googleAuth()
-        setInnerHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Select a Chat to see the messages!</label><div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">Logout</label></div>`);
-        loadChats();
+        setInnerHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Select a Conversation to see the messages!</label><div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">Logout</label></div>`);
+        loadConversations();
     } else {
         deleteCookie("token");
         deleteCookie("username");
-        setInnerHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Login to see chats and messsages!</label><div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">Login</label></div>`)
-        clearChats();
+        setInnerHtml("personTo", `<label style="margin:0 10px;font-size:20px;">Login to see conversations and messsages!</label><div class="d-flex flex-fill" style="width:0;"></div><label style="margin:0 30px;font-size:20px;" onclick="toggleLogin()" id="loginBtn">Login</label></div>`)
+        clearConversations();
         clearMessages();
     }
 
@@ -164,13 +175,13 @@ function clearMessages() {
     setInnerHtml("messages", "")
 }
 
-function clearChats() {
-    chats = [];
-    setInnerHtml("chats", "")
+function clearConversations() {
+    conversations = [];
+    setInnerHtml("conversations", "")
 }
 
 function googleAuth() {
-    //Todo: google auth here set token and username
+    // TODO: google auth here set token and username
 
     let token = "1234"
     let username = "Duncan"
@@ -179,40 +190,34 @@ function googleAuth() {
 }
 
 function addSmiley() {
-    document.getElementById("messageToSend").value += ":)";
+    document.getElementById("messageToSend").value += ":)"; // Brilliant XD
 }
 
-function recieve_message(message){
-     chats.forEach(chat => {
-        if(chat.chatId === message.chatId){
-            chat.messages.push(message);
+function recieve_message(message) {
+     conversations.forEach(conversation => {
+        if(conversation.conversationId === message.conversationId){
+            conversation.messages.push(message);
         }
      });
 
-     if(message.chatId === currentChat){
+     if(message.conversationId === currentConversation){
         displayMessage(message)
      }
 }
 
-function recieve_chat(chat){
-    chats.push(chat)
+function recieve_conversation(conversation) {
+    conversations.push(conversation)
     let username = getCookie("username");
-    let person = username === chat.user1 ? chat.user2 : chat.user1;
-    chatHtml = `
-    <div class="d-flex align-items-center" style="height:10%;width:100%;" onclick="loadMessages(${chat.chatId})">
-        <img src="assets/img/profile_picture2.png" style="width:40px;height:40px;margin:0 10px 0 20px;">
-        <label style="margin:0 10px;font-size:20px;">${person}</label>
-   </div>
-   <hr>`
-    prependHtml("chats", chatHtml);
+    let person = username === conversation.user1 ? conversation.user2 : conversation.user1;
+    prependHtml("conversations", getConversationHtml(conversation.conversationId, person));
 }
 
-function send_chat(){
+function send_conversation() {
 
 }
 
 function send_message() {
-    let currentChatId = currentChat;
+    let currentConversationId = currentConversation;
     let message = document.getElementById("messageToSend").value;
     let username = getCookie("username");
     console.log(`sending message: ${message}`)
@@ -220,14 +225,14 @@ function send_message() {
     let date = formatDate(new Date());
 
     let messageObj = {
-        chatId:currentChatId,
+        conversationId:currentConversationId,
         sender: username,
         description: message,
         timestamp: date
     }
-    chats.forEach(chat => {
-        if(chat.chatId === currentChatId){
-            chat.messages.push(message)
+    conversations.forEach(conversation => {
+        if(conversation.conversationId === currentConversationId){
+            conversation.messages.push(message)
         }
     })
     displayMessage(messageObj);
@@ -254,11 +259,11 @@ function deleteCookie(cname) {
     window.localStorage.removeItem(cname);
 }
 
-function prependHtml(id, html){
+function prependHtml(id, html) {
     document.getElementById(id).innerHTML = html + document.getElementById(id).innerHTML;
 }
 
-function appendHtml(id, html){
+function appendHtml(id, html) {
     document.getElementById(id).innerHTML = document.getElementById(id).innerHTML + html;
 }
 
